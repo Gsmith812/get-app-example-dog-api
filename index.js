7a38de61d3c264d78290963868996003b1d58fa5
George Smith <georgesmith812@gmail.com>
2020-09-16 00:00:50 -0400

    app is fully functional

---
+++ index.js
@@ -0,0 +1,31 @@
+let numberOfDogPictures = 3;
+
+function displayDogImgs(item) {
+    $(".results").append(`<img src="${item}" alt="Picture of dog">`);   
+}
+
+function handleDogImgsArray(responseJson) {
+    arrayOfDogImgs = responseJson.message;
+    return arrayOfDogImgs.map(item => displayDogImgs(item));
+}
+
+function getDogImages() {
+    fetch(`https://dog.ceo/api/breeds/image/random/${numberOfDogPictures}`)
+    .then(response => response.json())
+    .then(responseJson => handleDogImgsArray(responseJson));
+}
+
+function handleSubmitClicked() {
+    $("form").submit(event => {
+        event.preventDefault();
+        $(".results").empty();
+        numberOfDogPictures = $("input").val();
+        $(".results").append("<h2>Here are those fur babies you requested!</h2>")
+        getDogImages();
+    });
+}
+
+$(function() {
+    console.log("Application Loaded, Now Ready for Submit!");
+    handleSubmitClicked();
+});
